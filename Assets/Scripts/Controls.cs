4c69dea8f9e6b2576d3b075f4acde611e0109073
GabeDev <gabriel.d.albu@gmail.com>
2019-06-11 12:38:31 -0400

    First Commit

---
+++ Assets/Scripts/Controls.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : MonoBehaviour
+{
+    public float minSwipeDistY;
+ 
+    public float minSwipeDistX;
+    public Rigidbody rb;
+         
+    private Vector2 startPos;
+ 
+    public float thrust;
+    void Update()
+    {
+//#if UNITY_ANDROID
+        if (Input.touchCount > 0) 
+             
+        {
+             
+            Touch touch = Input.touches[0];
+             
+             
+             
+            switch (touch.phase) 
+                 
+            {
+                 
+            case TouchPhase.Began:
+ 
+                startPos = touch.position;
+                 
+                break;
+                 
+                 
+                 
+            case TouchPhase.Ended:
+ 
+                   
+                     
+                    float swipeDistHorizontal = (new Vector3(touch.position.x,0, 0) - new Vector3(startPos.x, 0, 0)).magnitude;
+                     
+                    if (swipeDistHorizontal > minSwipeDistX) 
+                         
+                    {
+                         
+                        float swipeValue = Mathf.Sign(touch.position.x - startPos.x);
+                         
+                        if (swipeValue > 0)
+                        {
+                             rb.AddForce(thrust, 0,0, ForceMode.Impulse);
+                        }
+                             
+                            
+                         
+                        else if (swipeValue < 0)
+                        {
+                            rb.AddForce(-thrust, 0,0, ForceMode.Impulse);
+                        }
+                             
+                            //MoveLeft ();
+                         
+                    }
+                break;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnObj.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObj : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject objToSpawn;
+    public Vector3 spawnValues;
+    public float spawnWait;
+    public float spawnLeastWait;
+    public float spawnMostWait;
+    public int startWait;
+
+    int randEnemy;
+    public bool stop;
+
+    void Start()
+    {
+        StartCoroutine(waitSpawner());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      spawnWait=Random.Range(spawnLeastWait,spawnMostWait);
+    }
+
+    IEnumerator waitSpawner()
+    {
+        while(!stop)
+        {
+       randEnemy=Random.Range(0,2);
+       Vector3 spawnposition = new Vector3(Random.Range(-3,3),1.5f,7);
+       Instantiate(objToSpawn,spawnposition,transform.rotation);
+        yield return new WaitForSeconds(spawnWait);
+        }
+    }
+}
