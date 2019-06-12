6a017b52ef42bbc966d06e4bbd22fc7f5224bb38
GabeDev <gabriel.d.albu@gmail.com>
2019-06-11 22:18:50 -0400

    Updated

---
+++ Assets/Scripts/SceneManagerScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneManagerScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SceneChangeMain()
+    {
+        SceneManager.LoadScene(1);
+
+    }
+     public void SceneChangeTutorial()
+    {
+        SceneManager.LoadScene(2);
+
+    }
+    public void quit()
+    {
+         Application.Quit();
+    }
+}
---
+++ Assets/Scripts/ScoreScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreScript : MonoBehaviour
+{
+    
+    float score=0;
+    bool stop = false;
+    public Text scoretext;
+    // Start is called before the first frame update
+    void Start()
+    {
+         StartCoroutine(scoreCounter());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+     IEnumerator scoreCounter()
+    {
+        while(!stop)
+        {
+       
+        yield return new WaitForSeconds(5);
+        score+=50;
+        scoretext.text=""+score;
+        }
+    }
+}
